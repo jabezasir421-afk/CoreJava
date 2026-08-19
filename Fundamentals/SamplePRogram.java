c80ee6110648fd04978f0e27fbd70b0b923bcc8e
jabezasir421-afk <jabezasir421@gmail.com>
2026-08-20 01:22:10 +0530

    Initial Commit

---
+++ Fundamentals/SamplePRogram.java
@@ -0,0 +1,13 @@
+package Fundamentals;
+
+import java.lang.*;
+
+/**
+ * SamplePRogram
+ */
+public class SamplePRogram {
+
+   public static void main(String[] args) {
+    IO.println("Hello World");
+   }
+}
