f0311923ff1fbfd75430b465d43cbf73c5898d91
jabezasir421-afk <jabezasir421@gmail.com>
2026-08-23 02:12:46 +0530

    restored accidentally deleted files

--- Fundamentals/ConditionalStatements.java
+++ Fundamentals/ConditionalStatements.java
@@ -1,17 +1,25 @@
 package Fundamentals;
 
+import Fundamentals.Performance;
+
 public class ConditionalStatements
 {
 
+    private Performance performance;
     private final int target=10000;
-     void main() {
+
+    public static void main() {
 
         int sales= Integer.parseInt(IO.readln("Enter the amount : "));
 
     }
 
     int bonus(int sales){
-         if(sales>target){
+
+         int bonus = 0;
+         if(sales>2*target){
+             performance=Performance.
+
 
          }
     }
--- Fundamentals/IncrementAndDecrementOperators.java
+++ Fundamentals/IncrementAndDecrementOperators.java
@@ -13,4 +13,5 @@ public class IncrementAndDecrementOperators {
         System.out.println(y);
     }
 
+
 }
