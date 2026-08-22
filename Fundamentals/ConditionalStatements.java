23e176eb50e389c8f24b44168eb9f23cb3a83c0b
jabezasir421-afk <jabezasir421@gmail.com>
2026-08-23 02:18:11 +0530

    If else-if

--- Fundamentals/ConditionalStatements.java
+++ Fundamentals/ConditionalStatements.java
@@ -1,16 +1,17 @@
 package Fundamentals;
 
-import Fundamentals.Performance;
+
 
 public class ConditionalStatements
 {
 
     private Performance performance;
     private final int target=10000;
-
-    public static void main() {
+ void main() {
 
         int sales= Integer.parseInt(IO.readln("Enter the amount : "));
+
+        int bonus= bonus(sales);
 
     }
 
@@ -18,10 +19,22 @@ public class ConditionalStatements
 
          int bonus = 0;
          if(sales>2*target){
-             performance=Performance.
+             performance=Performance.Excelent;
+             System.out.println(performance);
+             bonus=1000;
+         } else if (sales>=1.5*target) {
+             performance=Performance.Great;
+             bonus=500;
 
-
+         } else if (sales>=target) {
+             performance=Performance.Satisfacoty;
+             System.out.println(performance);
+             bonus=100;
+         }else {
+             System.out.println("You are fired");
          }
+
+         return bonus;
     }
 
 
